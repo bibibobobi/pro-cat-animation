4df786e46d62bccfd4f2bfa8b79a0145ae7279f0
bibibobobi <smilingcat33@gmail.com>
2022-08-11 18:20:09 +0800

    add mbVideo

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 gsap.registerPlugin(ScrollTrigger);
-const bgVvideo = document.querySelector('#bgVideo');
+const bgVideo = document.querySelector('#bgVideo');
+const mbVideo = document.querySelector('#mbVideo');
 bgVideo.pause();
+mbVideo.pause();
 bgVideo.currentTime = 0;
+mbVideo.currentTime = 0;
+
 let sections = gsap.utils.toArray('.step');
 sections.forEach((step, i) => {
   ScrollTrigger.create({
@@ -26,10 +30,25 @@ sections.forEach((step, i) => {
       ease: 'none',
     }
   );
+  gsap.fromTo(
+    mbVideo,
+    { currentTime: 3 * i },
+    {
+      scrollTrigger: {
+        trigger: step,
+        scrub: 2,
+        start: 'top bottom',
+        end: 'bottom bottom',
+      },
+      currentTime: 3 * (i + 1),
+      duration: 1,
+      ease: 'none',
+    }
+  );
 });
-gsap.to('#bgVideo', {
-  scrollTrigger: {
-    scrub: true,
-  },
-  scale: 1.5,
-});
+// gsap.to('#bgVideo', {
+//   scrollTrigger: {
+//     scrub: true,
+//   },
+//   scale: 1.5,
+// });
