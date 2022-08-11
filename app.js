45b476d73f6b1da2b8863ed199d07b367bc544d3
bibibobobi <smilingcat33@gmail.com>
2022-08-11 16:56:20 +0800

    How to control video with ScrollTrigger | GSAP Animation

--- app.js
+++ app.js
@@ -1,44 +1,35 @@
-const intro = document.querySelector('.intro');
-const video = intro.querySelector('video');
-const text = intro.querySelector('h1');
-//END SECTION
-const section = document.querySelector('section');
-const end = section.querySelector('h1');
-
-//SCROLL MAGIC
-const controller = new ScrollMagic.Controller();
-
-//Scenes
-let scene = new ScrollMagic.Scene({
-  duration: 9000,
-  triggerElement: intro,
-  triggerHook: 0,
-})
-  //.addIndicators()
-  .setPin(intro)
-  .addTo(controller);
-
-//Text Animation
-const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
-
-let scene2 = new ScrollMagic.Scene({
-  duration: 3000,
-  triggerElement: intro,
-  triggerHook: 0,
-})
-  .setTween(textAnim)
-  .addTo(controller);
-
-//Video Animation
-let accelamount = 0.1;
-let scrollpos = 0;
-let delay = 0;
-
-scene.on('update', (e) => {
-  scrollpos = e.scrollPos / 1000;
+gsap.registerPlugin(ScrollTrigger);
+const bgVvideo = document.querySelector('#bgVideo');
+bgVideo.pause();
+bgVideo.currentTime = 0;
+let sections = gsap.utils.toArray('.step');
+sections.forEach((step, i) => {
+  ScrollTrigger.create({
+    trigger: step,
+    start: 'bottom bottom',
+    end: '+=1000',
+    pin: true,
+    anticipatePin: 1,
+  });
+  gsap.fromTo(
+    bgVideo,
+    { currentTime: 3 * i },
+    {
+      scrollTrigger: {
+        trigger: step,
+        scrub: 2,
+        start: 'top bottom',
+        end: 'bottom bottom',
+      },
+      currentTime: 3 * (i + 1),
+      duration: 1,
+      ease: 'none',
+    }
+  );
 });
-
-setInterval(() => {
-  delay += (scrollpos - delay) * accelamount;
-  video.currentTime = delay;
-}, 33.3);
+gsap.to('#bgVideo', {
+  scrollTrigger: {
+    scrub: true,
+  },
+  scale: 1.5,
+});
